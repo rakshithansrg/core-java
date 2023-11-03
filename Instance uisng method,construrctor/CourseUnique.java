6df641a4a38f14910a06fa3983dd9fdef126ef7b
rakshithansrg <rakshithansrg.xworkz@gmail.com>
2023-11-03 10:09:13 +0530

    Instance Method

---
+++ Instance uisng method,construrctor/CourseUnique.java
@@ -0,0 +1,70 @@
+class CourseUnique
+{
+	public static void main(String[] args)
+	{
+		 System.out.println("Start main in CourseUnique");
+		 
+		 Course course1=new Course("java",5,6,24000,"BTM Layout Bangalore",4.5,true);
+		 course1.displayCourse();
+		 
+		 System.out.println("======================");
+		 
+		 Course course2=new Course("Devops",5,4,35000,"RR nagar Bangalore",5,true);
+		 course2.displayCourse();
+		 
+		 System.out.println("=======================");
+		 
+		 Course course3=new Course("Data Science",6,7,60000,"Kuvempu nagar Blr",5,false);
+		 course3.displayCourse();
+		 
+		 System.out.println("========================");
+		 
+		 Course course4=new Course("Testing",3,6,28000,"JP nagar Bangalore",4,true);
+		 course4.displayCourse();
+		 
+		 System.out.println("===========================");
+		 
+		 Course course5=new Course("Python",6,5,30000,"MG road Bangalore",5,false);
+		 course5.displayCourse();
+		 
+		 System.out.println("==========================");
+		 
+		 Course course6=new Course("Cyber Security",5,8,40000,"Rajajinagar Bangalore",4.5,true);
+		 course6.displayCourse();
+		 
+		 System.out.println("===========================");
+		 
+		 Course course7=new Course("Cloud",6,3,45000,"Hebbal",4.5,true);
+		 course7.displayCourse();
+		 
+		 System.out.println("===========================");
+		 
+		 Course course8=new Course("Big Data",5,4,50000,"Banashankri",4,false);
+		 course8.displayCourse();
+		 
+		 System.out.println("===========================");
+		 
+		 Course course9=new Course("UI/UX",6,7,35000,"Jayanagar",4.6,true);
+		 course9.displayCourse();
+		 
+		 System.out.println("===========================");
+		 
+		 Course course10=new Course(".net",6,8,25000,"Marth halli",3,false);
+		 course10.displayCourse();
+		 
+		 System.out.println("===========================");
+		 
+		 Course course11=new Course("AWS",5,4,32000,"Kormangla",4,false);
+		 course11.displayCourse();
+		 
+		 System.out.println("===========================");
+		 
+		 Course course12=new Course("Data Analytics",3,4,38000,"Shanthinagar",4,false);
+		 course2.displayCourse();
+		 
+		 
+		 System.out.println("End main in CourseUnique");
+
+		 
+    }
+}
