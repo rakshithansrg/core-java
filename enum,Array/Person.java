8b28dcb04180edc057f2fab7da43093744f6b316
rakshithansrg <rakshithansrg.xworkz@gmail.com>
2023-11-08 10:49:17 +0530

    Using array

--- enum,Array/Person.java
+++ enum,Array/Person.java
@@ -3,33 +3,31 @@ class Person
 	String name;
 	Email[] email;
 	
-	Person()
-	{
-		System.out.println("Start default const");
-	}
-	
-    void setName(String name)
+	Person(String name, Email[] email)
 	{
 		this.name=name;
-		
-	}
-	void setMail(Email[] email)
-	{
 		this.email=email;
-	}
 	
+    }	
 	void showInfo()
 	{
 		System.out.println("Name : "+this.name);
 		System.out.println("Email :"+this.email);
 		if(this.email!=null)
 		{
+			System.out.println("Email length:"+this.email.length);
 			for(int i=0;i<this.email.length;i++)
 			{
-			    Email ref=this.email[i];
-				System.out.println("Ref :"+ref );
+			    Email emailArray=this.email[i];
+				System.out.println("Email  :"+emailArray );
+				emailArray.showInfo();
 				
 			}
 		}
+		System.out.println("Starting info in Person");
 	}
 }
+	
+	
+	
+	
