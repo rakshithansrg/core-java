0d69c23f81904280ac95a02dcc9d086088a33811
rakshithansrg <rakshithansrg.xworkz@gmail.com>
2023-11-09 23:03:26 +0530

    data type association custom type

---
+++ Datatype inst/City.java
@@ -0,0 +1,15 @@
+class City
+{
+	String name="DVG";
+    String stateName="Karnataka";
+	
+		
+	void show()
+	{
+		System.out.println("----City----");
+		System.out.println("City :"+this.name);
+		System.out.println("State Name :"+this.stateName);
+	}
+	
+	
+}
