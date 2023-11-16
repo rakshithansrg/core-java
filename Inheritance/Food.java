285d42057fc0a175756fb39d2e17d857977e00a1
rakshithansrg <rakshithansrg.xworkz@gmail.com>
2023-11-16 23:51:28 +0530

    Inheritance with no arg constructor r default

---
+++ Inheritance/Food.java
@@ -0,0 +1,7 @@
+class Food
+{
+	Food()
+	{
+		System.out.println("Starting from Food");
+	}
+}
---
+++ Inheritance/Sweet.java
@@ -0,0 +1,7 @@
+class Sweet extends Food
+{
+	Sweet()
+	{
+		System.out.println("Starting from Sweet");
+	}
+}
