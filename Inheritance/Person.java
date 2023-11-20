ba271aee734b94a803e524e0da4338ce68b2efce
rakshithansrg <rakshithansrg.xworkz@gmail.com>
2023-11-20 23:17:44 +0530

    creating instance and typecasting

---
+++ Inheritance/Person.java
@@ -0,0 +1,17 @@
+class Person
+{
+	Person()
+	{
+		System.out.println("Starting from Person");
+	}
+	
+	void eat()
+	{
+		System.out.println("Starting eat in Person");
+	}
+	
+	void sleep()
+	{
+		System.out.println("Starting code in sleep");
+	}
+}
