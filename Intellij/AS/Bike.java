457eade225591da485e259c71b3a38311c4ad1e1
rakshithansrg <rakshithansrg.xworkz@gmail.com>
2023-11-29 09:51:09 +0530

    AS

---
+++ Intellij/AS/Bike.java
@@ -0,0 +1,28 @@
+package com.Xworks.Vehicle.Car;
+
+public class Bike {
+
+    public Bike()
+    {
+        System.out.println("no arg constructor");
+    }
+
+   protected void useBike()
+    {
+        System.out.println("using method in bike");
+    }
+    public void setBike()
+    {
+        System.out.println("using method in setbike");
+
+    }
+    private void scooter()
+    {
+        System.out.println("using method in scooter");
+    }
+    void hondaShine()
+    {
+        System.out.println("using method in hondashine");
+
+    }
+}
